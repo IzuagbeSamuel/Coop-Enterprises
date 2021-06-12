5c6d32d6fa453e216f37085a5d68206d0c93f23c
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-06-12 03:31:43 +0100

    Changed search box

--- js/index.js
+++ js/index.js
@@ -1,27 +1,42 @@
-// const searchBox = document.getElementById('search');
-// searchBox.addEventListener('click', showBlogs);
-// function showBlogs() {
-//   // console.log('Search BOx Clicked');
-//   let searchContent = document.getElementById('content');
-//   searchContent.innerHTML = `<li><a href="more1.html">Best Tools and Deals for Developers and Designers</a></li>
-//   <li><a href="more2.html">Powerful Techniques For Responsive Web Design</a></li>
-//   <li><a href="more3.html">CSS Vertical Alignment: Tips and Techniques</a></li>
-//   <li><a href="more4.html">Quick Tips to make your WordPress Site Faster</a></li>
-//   <li><a href="more5.html">Introduction to GitHub and Git Bash</a></li>
-//   <li><a href="more6.html">Best Visual Studio Code Extensions</a></li>
-//   <li><a href="more7.html">Javascript Event Listeners</a></li>
-//   <li><a href="more8.html">Ways To earn Money as a Developer</a></li>`;
-// }
+const searchWrapper = document.querySelector('.search-input');
+const inputBox = searchWrapper.querySelector('input');
+const suggBox = searchWrapper.querySelector('.autocom-box');
 
-// window.onclick = function (e) {
-//   if (!e.target.matches('#search')) {
-//     var blogs = searchContent.innerHTML;
-//     var i;
-//     for (i = 0; i < blogs.length; i++) {
-//       var openBlog = blogs[i];
-//       if (openBlog.contain('li')) {
-//         openBlog.remove('li');
-//       }
-//     }
-//   }
-// };
+inputBox.onkeyup = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = suggestions.filter((data) => {
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return (data = '<li>' + data + '</li>');
+    });
+    console.log(emptyArray);
+    showSuggestions(emptyArray);
+    let allList = suggBox.querySelectorAll('li');
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute('onclick', 'select(this)');
+    }
+    searchWrapper.classList.add('active');
+  } else {
+    searchWrapper.classList.remove('active');
+  }
+};
+
+function select(element) {
+  let selectUserData = element.textContent;
+  inputBox.value = selectUserData;
+  searchWrapper.classList.remove('active');
+}
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = inputBox.value;
+    listData = '<li>' + userValue + '</li>';
+  } else {
+    listData = list.join('');
+  }
+  suggBox.innerHTML = listData;
+}
