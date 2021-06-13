0a70232e701f3a60bb98032d1140711aa624f64a
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-06-13 02:58:48 +0100

    add side navbar

--- js/index.js
+++ js/index.js
@@ -40,3 +40,13 @@ function showSuggestions(list) {
   }
   suggBox.innerHTML = listData;
 }
+
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('alert').style.marginRight = '250px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('alert').style.marginRight = '0';
+}
