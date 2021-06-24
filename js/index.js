2416b109cbec31f68705c9fd3a80718968b965d3
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-06-24 11:43:02 +0100

    add styled to dropdown on my side bar

--- js/index.js
+++ js/index.js
@@ -42,8 +42,8 @@ function showSuggestions(list) {
 }
 
 function openSlideMenu() {
-  document.getElementById('side-menu').style.width = '250px';
-  document.getElementById('alert').style.marginRight = '250px';
+  document.getElementById('side-menu').style.width = '300px';
+  document.getElementById('alert').style.marginRight = '300px';
 }
 
 function closeSlideMenu() {
@@ -55,6 +55,21 @@ function closeEmailCnt() {
   document.getElementById('email-content').style.display = 'none';
 }
 
+function showDropdown() {
+  document.getElementById('down-arrow').style.display = 'none';
+  document.getElementById('dropdown-content').style.display = 'block';
+  document.getElementById('hide-dropdown').style.display = 'flex';
+}
+
+function closeDropdown() {
+  document.getElementById('dropdown-content').style.display = 'none';
+  document.getElementById('hide-dropdown').style.display = 'none';
+  document.getElementById('down-arrow').style.display = 'block';
+}
+
+function closeTimeFrame() {
+  document.getElementById('time-frame').style.display = 'none';
+}
 // let emailCnt = document.getElementById('email-content');
 // window.onscroll = function () {
 //   scrollFunction();
