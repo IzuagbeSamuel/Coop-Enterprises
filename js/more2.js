11cffd58521c707c23af919e366b23b1a614dc66
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-06-25 15:10:15 +0100

    changed style on all tabs

---
+++ js/more2.js
@@ -0,0 +1,34 @@
+
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '300px';
+  document.getElementById('alert').style.marginRight = '300px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  // document.getElementById('alert').style.marginRight = '0';
+}
+
+function closeEmailCnt() {
+  document.getElementById('email-content').style.display = 'none';
+}
+
+function showDropdown() {
+  document.getElementById('down-arrow').style.display = 'none';
+  document.getElementById('dropdown-content').style.display = 'block';
+  document.getElementById('hide-dropdown').style.display = 'flex';
+}
+
+function closeDropdown() {
+  document.getElementById('dropdown-content').style.display = 'none';
+  document.getElementById('hide-dropdown').style.display = 'none';
+  document.getElementById('down-arrow').style.display = 'block';
+}
+
+function closeTimeFrame() {
+  document.getElementById('time-frame').style.display = 'none';
+}
+
+function closeMessage() {
+  document.getElementById('callout-message').style.display = 'none';
+}
