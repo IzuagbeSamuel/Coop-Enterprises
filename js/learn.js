24d976f4a82654cc79f4ad5d45003a293bb18903
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-07-01 12:46:30 +0100

    added key token to ads

--- js/learn.js
+++ js/learn.js
@@ -6,3 +6,26 @@ function closeMessage() {
 
 
 
+// function grade(mark) {
+//   let grade = "";
+//   if(mark < 39) {
+//     grade = "F9";
+//   } else if(mark <= 44) {
+//     grade = "E8";
+//   } else if(mark <= 49) {
+//     grade = "D7"; 
+//   } else if(mark <= 54) {
+//     grade = "C6";
+//   } else if(mark <= 59) {
+//     grade = "C5";
+//   } else if(mark <= 64) {
+//     grade = "C4";
+//   } else if(mark <= 69) {
+//     grade = "B3";
+//   } else if(mark <= 74) {
+//     grade = "B2";
+//   } else if(mark >= 75) {
+//     grade = "A1"
+//   }
+//   return grade
+// }
