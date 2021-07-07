9da7141ea30380e2ccf99344643eff67b6dd864d
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-07-07 13:14:50 +0100

    styled the sixth tab

--- js/more6.js
+++ js/more6.js
@@ -5,11 +5,6 @@ function openSlideMenu() {
 
 function closeSlideMenu() {
   document.getElementById('side-menu').style.width = '0';
-  // document.getElementById('alert').style.marginRight = '0';
-}
-
-function closeEmailCnt() {
-  document.getElementById('email-content').style.display = 'none';
 }
 
 function showDropdown() {
@@ -22,12 +17,4 @@ function closeDropdown() {
   document.getElementById('dropdown-content').style.display = 'none';
   document.getElementById('hide-dropdown').style.display = 'none';
   document.getElementById('down-arrow').style.display = 'block';
-}
-
-function closeTimeFrame() {
-  document.getElementById('time-frame').style.display = 'none';
-}
-
-function closeMessage() {
-  document.getElementById('callout-message').style.display = 'none';
 }
